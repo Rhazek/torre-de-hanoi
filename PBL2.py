de1975b87b7038b0decac7b7cd3a92ceb5743158
Matheus Rhazek Lira Santos <matheus_rhazek@hotmail.com>
2023-09-11 15:55:48 -0300

    Melhorando

--- PBL2.py
+++ PBL2.py
@@ -1,19 +1,10 @@
 import os
 
-def mostrarTorres(torres):
-    for i, torre in enumerate(torres):
-        print(f"Torre {i + 1}: {torre}")
-
-def moverDisco(torres, origem, destino):
-    if torres[origem] == []:
-       print("A torre de origem está vazia.")
-    elif (torres[destino] == []) or (torres[origem][-1] < torres[destino][-1]):
-        disco = torres[origem].pop()
-        torres[destino].append(disco)
-    else:
-        print("Movimento inválido. Não é possível colocar um disco maior em cima de um menor.")
-
 def torreDeHanoi():
+    print('''====================================================
+                    TORRE DE HANOI                   
+====================================================''')
+
     n = int(input("Com quantos discos você gostaria de ser desafiado? "))
 
     if n <= 0:
@@ -30,7 +21,7 @@ def torreDeHanoi():
         origem = int(input("\nDigite a torre de origem (1, 2, 3): ")) - 1
         destino = int(input("Digite a torre de destino (1, 2, 3): ")) - 1
         os.system('cls' if os.name == 'nt' else 'clear')
-
+        
         if origem not in (0, 1, 2) or destino not in (0, 1, 2):
             print("Torre inválida. Use 1, 2 ou 3.")
             continue
@@ -39,7 +30,21 @@ def torreDeHanoi():
 
         if torres[2] == list(range(n, 0, -1)):
             mostrarTorres(torres)
-            print("Parabéns! Você venceu a Torre de Hanoi!")
+            print("\nParabéns! Você venceu a Torre de Hanoi!")
             break
+
+def mostrarTorres(torres):
+    for i in range(len(torres)):
+        torre = torres[i]
+        print(f"Torre {i + 1}: {torre}")
+
+def moverDisco(torres, origem, destino):
+    if torres[origem] == []:
+       print("A torre de origem está vazia.\n")
+    elif (torres[destino] == []) or (torres[origem][-1] < torres[destino][-1]):
+        disco = torres[origem].pop()
+        torres[destino].append(disco)
+    else:
+        print("Movimento inválido. Não é possível colocar um disco maior em cima de um menor.")
 
 torreDeHanoi()
